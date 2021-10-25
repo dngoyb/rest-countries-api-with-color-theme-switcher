a1a07682e72a645a2dcf4ac296d5af34aa4a0f89
Ngoy banza <dngoyb@gmail.com>
2021-10-25 17:33:46 +0200

    Limiting number of countries loaded

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
 const container = document.querySelector('.container');
 const input = document.querySelector('.search__input');
 
+let posts = [];
+let now = [];
+const maxDisplayLimit = 16;
+const frag = document.createDocumentFragment();
+let arr = [];
+
+const loadPosts = () => {};
+
 const getData = () => {
 	axios
 		.get('https://restcountries.com/v3.1/all')
 
 		.then((res) => {
 			res.data.forEach(
-				({ name: { common }, population, region, capital, flags: { png } }) => {
-					let div = document.createElement('div');
-					div.classList.add('card');
+				(
+					{ name: { common }, population, region, capital, flags: { png } },
+					index
+				) => {
+					if (index < maxDisplayLimit) {
+						let div = document.createElement('div');
+						div.classList.add('card');
+						div.innerHTML = `
 
-					div.innerHTML = `
-
-                <img src="${png}" alt="" class="card__img" />
-                    <div class="card__desc">
-                        <h4 class="card__name">${common}</h4>
+	            <img src="${png}" alt="" class="card__img" />
+	                <div class="card__desc">
+	                    <h4 class="card__name">${common}</h4>
 				        <p class="card__populations">Population: <small>${numberWithCommas(
 									population
 								)}</small></p>
 				        <p class="card__region">Region: <small>${region}</small></p>
 				        <p class="card__capital">Capital: <small>${capital}</small></p>
-                    </div>
-                `;
-					container.appendChild(div);
+	                </div> 
+	            `;
+						container.append(div);
+					}
 				}
 			);
 		});
 };
-
-// const searchData = () => {
-//     axios.get('https://restcountries.com/v3.1/name/{name}');
-// }
 
 function numberWithCommas(x) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
